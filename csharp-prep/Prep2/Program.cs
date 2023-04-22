4941d3217640520f7771c970ed3970d2f110cbc0
vanceshih <vanceshih@gmail.com>
2023-04-23 02:06:31 +0800

    W01 Prove

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage. ");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+
+        string letter = "null";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }       
+        
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }          
+        
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }    
+        
+        else
+        {
+            letter = "F";
+        }    
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+            if (grade >= 70)
+            {
+                Console.WriteLine("Congratulation! You have successfully passed this class.");
+            }  
+
+            else
+            {
+                Console.WriteLine("Unfortunately, you have not met the requirements to pass this class. You may try again in the future.");
+            }      
+
     }
 }
